c057462601a67be847fba88c56e2963e1a8dc046
Sila <56313448+ChSila@users.noreply.github.com>
2020-09-17 16:08:39 +0700

    Facedetection
    Code for facedetection v 1.0

---
+++ FaceDetection.py
@@ -0,0 +1,17 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+while True:
+    _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    cv2.imshow('Frames', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
